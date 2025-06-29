0226eb23f4981f72ef019d6012e70175291909d4
Raj-S-Rawat <rsrawat2179@gmail.com>
2025-06-29 23:40:13 +0530

    new story add krdiya (#1)
    Co-authored-by: rajendra rawat <rsrawat219@gmail.com>

--- main.tf
+++ main.tf
@@ -9,3 +9,7 @@ resource "azurerm_key_vault_secret" "secrets" {
   value        = "Devopsinsiders"
   key_vault_id = data.azurerm_key_vault.keyvault.id
 }
+
+# This Terraform configuration retrieves an existing Azure Key Vault and stores a secret in it.
+# The Key Vault is identified by its name "Devops123keyV" in the resource group "AJ".
+# A secret named "username" with the value "Devopsinsiders" is added to the Key Vault.
